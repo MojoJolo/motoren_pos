da45520e92fdef57ed9452bbef6a0cafc5acca84
Jolo Balbin <jolo@jpbalb.in>
2016-06-04 00:58:35 +0800

    added basic inventory methods

--- app/db.py
+++ app/db.py
@@ -10,3 +10,33 @@ class Db:
           use_unicode=True, charset="utf8", cursorclass=MySQLdb.cursors.DictCursor)
 
         self.cursor = self.db.cursor()
+
+    def add_inventory(self, item):
+        query = """INSERT INTO inventories (name, description, code, stock, price, supplier)
+                    VALUES (%s, %s, %s, %s, %s, %s)"""
+
+        self.cursor.execute(query, item)
+
+        self.db.commit()
+        self.db.close()
+
+    def search_inventory(self, q):
+        q = "%" + q + "%"
+        query = """SELECT * from inventories where name like %s or description like %s"""
+
+        self.cursor.execute(query, (q, q))
+        self.db.close()
+
+        results = self.cursor.fetchall()
+
+        return list(results)
+
+    def get_inventories(self):
+        query = """SELECT * from inventories"""
+
+        self.cursor.execute(query)
+        self.db.close()
+
+        results = self.cursor.fetchall()
+
+        return list(results)
--- app/index.py
+++ app/index.py
@@ -3,23 +3,57 @@ from flask import Flask, redirect,render_template, jsonify, request, url_for
 from app import app
 import json
 import arrow
+from db import Db
 
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template('index.html')
 
 @app.route("/inventory/add", methods=['GET', 'POST'])
-def addInventory():
+def add_inventory():
     if request.method == 'POST':
         name = request.form['name'] if 'name' in request.form else None
         description = request.form['description'] if 'description' in request.form else ''
         code = request.form['code'] if 'code' in request.form else None
-        price = request.form['price'] if 'price' in request.form else None
-        stock = request.form['stock'] if 'stock' in request.form else None
+
+        try:
+            price = int(request.form['price']) if 'price' in request.form else None
+        except:
+            return "Price is invalid!"
+
+        try:
+            stock = int(request.form['stock']) if 'stock' in request.form else None
+        except:
+            return "Stock is invalid"
+
+        supplier = request.form['supplier'] if 'supplier' in request.form else ''
 
         if name and code and price and stock:
-            return ""
+            item = (
+                name, description, code, stock, price, supplier
+            )
+
+            Db().add_inventory(item)
+
+            return "Success."
         else:
             return "Please complete required fields."
     else:
-        return "add inventory"
+        return "Add inventory"
+
+@app.route("/inventory/get_all", methods=['GET'])
+def get_inventories():
+    inventories = Db().get_inventories()
+
+    return json.dumps(inventories)
+
+@app.route("/inventory/search", methods=['GET'])
+def search_inventory():
+    q = request.args.get('q', '')
+
+    if q == '':
+        return redirect(url_for('index'))
+
+    results = Db().search_inventory(q)
+
+    return render_template('search.html', items=results)
